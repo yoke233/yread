22f0266b5b13f4cd75eafb1cfd0e89dccf232e48
yoke <yoke2012@gmail.com>
2013-06-24 21:09:53 +0800

    filter for dir

--- api/directory.js
+++ api/directory.js
@@ -4,27 +4,20 @@ var bookDao = yRead.dao.bookDao;
 function getDirectory(req, res, dm) {
     var _book_id = parseInt(req.path[2]);
     //book_id = bookDao.convertID(book_id);
-    var _book;
-    bookDao.getBookTitle(_book_id,dm.intercept(function (book) {
-        _book = book;
-    }));
-    articleDao.getDirectory(_book_id, dm.intercept(function (articlesList) {
-        if(articlesList){
-            var len = articlesList.length/4;
-            var _returnArr = [];
-            for(var i=0;i<len;i++){
-                _returnArr.push(articlesList.slice(i*4,i*4+4));
-            }
+    bookDao.getBookTitle(_book_id, dm.intercept(function (book) {
+        if (book) {
+            articleDao.getDirectory(_book_id, dm.intercept(function (articlesList) {
+                res.sendjson({
+                    book: book,
+                    array: articlesList
+                });
+            }));
+        } else {
             res.sendjson({
-                book:_book,
-                array:_returnArr
-            });
-        }else{
-            res.sendjson({
-                err:'no directory'
+                err: 'no directory'
             });
         }
-    }))
+    }));
 }
 
 function getFn(req, res, dm) {
--- web/js/filters.js
+++ web/js/filters.js
@@ -2,8 +2,19 @@
 
 /* Filters */
 
-angular.module('phonecatFilters', []).filter('checkmark', function() {
-    return function(input) {
-        return input ? '\u2713' : '\u2718';
-    };
-});
+angular.module('phonecatFilters', []).
+    filter('checkmark', function () {
+        return function (input) {
+            return input ? '\u2713' : '\u2718';
+        };
+    }).
+    filter('dirFilter', function () {
+        return function (input, num) {
+            var len = input.length / num;
+            var out = [];
+            for (var i = 0; i < len; i++) {
+                out.push(input.slice(i * num, i * num + num));
+            }
+            return out;
+        }
+    });
